70c212fbee01f827def3ee14deb5de1c8833394e
Chris Sullivan <chris.sullivan110@gmail.com>
2020-12-15 13:01:42 -0500

    day15

---
+++ src/day15/part1/index.js
@@ -0,0 +1,27 @@
+const tools = require('../../shared/datareader');
+
+tools.dataReader('C:/Repos/AoC2020/src/day15/input.txt', (err, data) => {
+    if(err) {
+        console.log('error reading file');
+        return;
+    }
+
+    const dataArray = data.split(',').map(x => +x);
+
+    const spokenNums = [];
+    for(let i = 0; i < 2020; i++) {
+        if(spokenNums.length < dataArray.length) {
+            spokenNums.push(dataArray[i]);
+            continue;
+        }
+
+        const lastIndexSpoken = spokenNums.slice(0, spokenNums.length - 1).lastIndexOf(spokenNums[spokenNums.length - 1]);
+        if(lastIndexSpoken < 0 ) {
+            spokenNums.push(0);
+        } else {
+            spokenNums.push((spokenNums.length - 1) - lastIndexSpoken)
+        }
+    }
+
+    console.log(`The 2020th number spoken is ${spokenNums[spokenNums.length -1]}`)
+});
---
+++ src/day15/part2/index.js
@@ -0,0 +1,31 @@
+const tools = require('../../shared/datareader');
+
+tools.dataReader('C:/Repos/AoC2020/src/day15/input.txt', (err, data) => {
+    if(err) {
+        console.log('error reading file');
+        return;
+    }
+
+    const dataArray = data.split(',').map(x => +x);
+    const lastSpokenMap = new Map();
+    const spokenNums = [];
+    for(let i = 0; i < 30000000; i++) {
+        if(spokenNums.length < dataArray.length) {
+            spokenNums.push(dataArray[i]);
+            lastSpokenMap.set(dataArray[i], i);
+            continue;
+        }
+        lastSpokenMap.set(spokenNums[spokenNums.length - 2], spokenNums.length - 2);
+        const lastIndexSpoken = lastSpokenMap.has(spokenNums[spokenNums.length - 1]) ?  lastSpokenMap.get(spokenNums[spokenNums.length - 1]) : - 1;
+        if(lastIndexSpoken < 0 ) {
+            spokenNums.push(0);
+        } else {
+            spokenNums.push((spokenNums.length - 1) - lastIndexSpoken)
+        }
+    }
+
+    console.log(`The 30000000th number spoken is ${spokenNums[spokenNums.length -1]}`)
+});
+
+
+/* TODO: Convert to using a map lookup for last index vs looping through array to find index */
